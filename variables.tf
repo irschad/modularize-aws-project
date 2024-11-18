7332a4acb42f0914f4fc7c1feed1a2db62abaac4
Irshad Nizami <irniz@rediffmail.com>
2024-11-18 09:58:40 +0530

    Modularize AWS tf project

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable env_prefix {}
+variable avail_zone {}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable my_ip {}
+variable instance_type {}
+variable public_key_location {}
+variable image_name {}
